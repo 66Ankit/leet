bb4e7e8c0bdaf34360881e22ed10e0d57a12ea6b
66Ankit <ankitjaglan66@gmail.com>
2020-12-24 10:56:27 +0530

    new files

---
+++ graph/all_paths_from_source_to_target.cpp
@@ -0,0 +1,53 @@
+// beats 15% other solutions in runtime
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+   
+   vector<vector<int>> res; // final result array
+   
+
+   // find path passing graph by reference
+   void findPath(vector<vector<int>> &graph,vector<bool> vis,int node,vector<int> temp)
+    {
+        if(node==graph.size()-1)
+        {
+            res.push_back(temp);
+            return ;
+        }
+            for(auto x:graph[node])
+            {
+                 if(vis[x]==false)
+                 {
+                     vis[x]=true;
+                     temp.push_back(x);
+                     findPath(graph,vis,x,temp);
+                     temp.pop_back();
+                     vis[x]=false;
+                 }
+            }
+        
+        return ;
+       
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> vis(n,false);
+        vector<int> temp;
+        temp.push_back(0);
+        vis[0]=true;
+        for(auto x:graph[0])
+        {   
+            vis[x]=true; // to keep track of visited nodes
+            temp.push_back(x);
+            findPath(graph,vis,x,temp);
+            vis[x]=false; // path from x node is explored now unvisit it .
+            temp.pop_back();  // path form x to final ndoe is explored , now remove x form temp.
+        }
+        
+        return(res);
+    }
+};
