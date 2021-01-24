2679472a13b9910518863e0df59016cef8979519
Ankit <ankit@Ankits-MacBook-Pro.local>
2021-01-24 14:45:45 +0530

    jan 25th 2021

---
+++ sliding_window/max_consecutive_1s.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        int n=A.size();
+        int left=0,right=0,k=0;
+        int res=0;
+        while(right<n)
+        {
+            if(A[right]==1)
+            {
+                right++;
+            }
+            else if(k!=K&&A[right]==0)
+            { 
+                k++;
+                right++;
+            }
+            else
+            {
+                while(A[left]==1)
+                    left++;
+                left++;
+                right++;
+            }
+            res=max(right-left,res);
+        }
+        return res;
+    }
+};
